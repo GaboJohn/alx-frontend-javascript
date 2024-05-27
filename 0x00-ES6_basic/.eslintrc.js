75bafce13ccdaeef3e6539e2d3658c36c206c0e2
GaboJohn <johngabotsalelwe@gmail.com>
2024-05-27 17:28:35 +0000

    Block Scope

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,21 +1,35 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es6: true,
-    jest: true
+    jest: true,
   },
-  extends: 'airbnb-base',
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
-    'no-var': 'error',
-    'prefer-const': 'error'
-  }
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
