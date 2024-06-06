7a29d78971ae67fc0bb898b033cb03ef5e6edc52
GaboJohn <johngabotsalelwe@gmail.com>
2024-06-06 19:03:31 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,11 @@
-const updateUniqueItems = (map) => {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  map.forEach((value, key) => {
-    if (value === 1) map.set(key, 100);
-  });
-};
-
-export default updateUniqueItems;
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  
+  for (const [key, value] of map.entries()) {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  }
+}
